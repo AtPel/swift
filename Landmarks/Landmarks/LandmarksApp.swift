73e3ff56b23798abf43c0f4ec7079550dcf3de44
Atte Peltonen <peltonenratte@gmail.com>
2022-01-31 09:45:51 +0200

    Projekti luotu

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Atte Peltonen on 31.1.2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
